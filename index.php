0e8a29ecaf0ca325051811e47cd5e024f5c6b2be
Shapeprogram <93039490+Shapeprogram@users.noreply.github.com>
2021-10-29 10:23:37 +0200

    Add files via upload

--- index.php
+++ index.php
@@ -9,36 +9,36 @@ if(file_exists($head))
     <h1>Church Program - 24 October 2021</h1>
     <div class="program">
         <h2>Program</h2>
-        <p><strong>Opening hymn </strong>: 123 Oh, May My Soul Commune with Thee</p>
+        <p class="fs-3"><strong>Opening hymn </strong>: 123 Oh, May My Soul Commune with Thee</p>
 
-        <p><strong>Opening prayer </strong> : By invitation</p>
+        <p class="fs-3"><strong>Opening prayer </strong> : By invitation</p>
 
-        <p><strong>Sacrament hymn </strong>: 170 God, Our Father, Hear Us Pray</p>
+        <p class="fs-3"><strong>Sacrament hymn </strong>: 170 God, Our Father, Hear Us Pray</p>
 
-        <p><strong>Speaker </strong>: Brother Kenneth Baami</p>
+        <p class="fs-3"><strong>Speaker </strong>: Brother Kenneth Baami</p>
 
-        <p><strong>Intermediate hymn </strong>: 266 The Time is Far Spent</p>
+        <p class="fs-3"><strong>Intermediate hymn </strong>: 266 The Time is Far Spent</p>
 
-        <p><strong>Speaker </strong>: Brother Doug Shepherd</p>
+        <p class="fs-3"><strong>Speaker </strong>: Brother Doug Shepherd</p>
 
-        <p><strong>Closing Hymn </strong>: 131 More Holiness Give Me</p>
+        <p class="fs-3"><strong>Closing Hymn </strong>: 131 More Holiness Give Me</p>
     </div>
 
     <div class="program">
         <h2>Announcements</h2>
-        <p><strong>31 Oct </strong>: Fifth Sunday lesson (Adults/Youth) second hour</p>
+        <p class="fs-3"><strong>31 Oct </strong>: Fifth Sunday lesson (Adults/Youth) second hour</p>
 
-        <p><strong>6 Nov </strong>: <br>- 16:00 to 18:00 Stake leadership Meeting <br>- 18:30 Adult Session</p>
+        <p class="fs-3"><strong>6 Nov </strong>: <br>- 16:00 to 18:00 Stake leadership Meeting <br>- 18:30 Adult Session</p>
 
-        <p><strong>7 Nov </strong>: <br>-10:00 to 12:00 Stake Conference Session for All <br>- 12:30 to 13:00 Temple and family history meeting (Temple /Family History leaders and consultants) <br>- 18:00 to 19:00 Fireside (Family History Devotional)</p>
+        <p class="fs-3"><strong>7 Nov </strong>: <br>- 10:00 to 12:00 Stake Conference Session for All <br>- 12:30 to 13:00 Temple and family history meeting (Temple /Family History leaders and consultants) <br>- 18:00 to 19:00 Fireside (Family History Devotional)</p>
 
-        <p><strong>Donations </strong>: We would like to share with you an email that Alice and Marcel Bastiaens from the Brussels Center Ward recently sent:
+        <p class="fs-3"><strong>Donations </strong>: We would like to share with you an email that Alice and Marcel Bastiaens from the Brussels Center Ward recently sent:
             “We would like to collect new or used toys from members, as well as Saint Nicholas and Christmas treats for affected children. “We will come and collect them in the units until November 15.”
         </p>
 
     </div>
     <div class="pdflink">
-        <p> If the website isn't clear enough, you can download the PDF version of the program here : <a href="./doc/24OCT21-QR.pdf" class="btn btn-outline-info" download="pdf-shape-program" role="button">Download the PDF</a></p>
+        <p class="fs-4"> If the website isn't clear enough, you can download the PDF version of the program here : <a href="./doc/24OCT21-QR.pdf" class="btn btn-outline-info" download="pdf-shape-program" role="button">Download the PDF</a></p>
     </div>
 
 <?php 
