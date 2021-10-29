889e939ed43b4c936f97fef34eae635f8c823adb
Shapeprogram <93039490+Shapeprogram@users.noreply.github.com>
2021-10-29 10:05:30 +0200

    Add files via upload

--- index.php
+++ index.php
@@ -9,28 +9,28 @@ if(file_exists($head))
     <h1>Church Program - 24 October 2021</h1>
     <div class="program">
         <h2>Program</h2>
-        <p><strong>Opening hymn </strong>: 272 Oh Say, What is Truth</p>
+        <p><strong>Opening hymn </strong>: 123 Oh, May My Soul Commune with Thee</p>
 
         <p><strong>Opening prayer </strong> : By invitation</p>
 
-        <p><strong>Sacrament hymn </strong>: 191 Behold The Great Redeemer Die</p>
+        <p><strong>Sacrament hymn </strong>: 170 God, Our Father, Hear Us Pray</p>
 
-        <p><strong>Speaker </strong>: Sister Samantha Baami</p>
+        <p><strong>Speaker </strong>: Brother Kenneth Baami</p>
 
-        <p><strong>Intermediate hymn </strong>: 274 The Iron Rod</p>
+        <p><strong>Intermediate hymn </strong>: 266 The Time is Far Spent</p>
 
-        <p><strong>Speaker </strong>: Brother John Riding</p>
+        <p><strong>Speaker </strong>: Brother Doug Shepherd</p>
 
-        <p><strong>Closing Hymn </strong>: 143 Let the Holy Spirit Guide</p>
+        <p><strong>Closing Hymn </strong>: 131 More Holiness Give Me</p>
     </div>
 
     <div class="program">
         <h2>Announcements</h2>
-        <p><strong>30 Oct </strong>: Branch Activity: Chili Cook-off/Trunk or Treat</p>
-
         <p><strong>31 Oct </strong>: Fifth Sunday lesson (Adults/Youth) second hour</p>
 
-        <p><strong>6-7 Nov </strong>: Stake Conference</p>
+        <p><strong>6 Nov </strong>: 16:00 to 18:00 Stake leadership Meeting - 18:30 Adult Session</p>
+
+        <p><strong>7 Nov </strong>: 10:00 to 12:00 Stake Conference Session for All - 12:30 to 13:00 Temple and family history meeting (Temple /Family History leaders and consultants) - 18:00 to 19:00 Fireside (Family History Devotional)</p>
 
         <p><strong>Donations </strong>: We would like to share with you an email that Alice and Marcel Bastiaens from the Brussels Center Ward recently sent:
             “We would like to collect new or used toys from members, as well as Saint Nicholas and Christmas treats for affected children. “We will come and collect them in the units until November 15.”
