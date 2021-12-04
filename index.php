c0a52a82244d46fd618d2067296a6103c236fa83
Shapeprogram <93039490+Shapeprogram@users.noreply.github.com>
2021-12-04 22:41:16 +0100

    Add files via upload

--- index.php
+++ index.php
@@ -6,29 +6,35 @@ if(file_exists($head))
 }
 ?>
 
-    <h1>Church Program - 28 November 2021</h1>
+    <h1>Church Program - 05 December 2021</h1>
     <div class="program">
         <h2>Program</h2>
-        <p class="fs-3"><strong>Opening hymn </strong>: 93 Prayer of Thanksgiving </p>
+        <p class="fs-3"><strong>Opening hymn </strong>: 202 Oh, Come, All Ye Faithful </p>
 
         <p class="fs-3"><strong>Opening prayer </strong> : By invitation</p>
 
-        <p class="fs-3"><strong>Sacrament hymn </strong>: 179 God, Our Father, Hear us Pray</p>
+        <p class="fs-3"><strong>Sacrament hymn </strong>: 146 Gently Raise the Sacred Strain</p>
 
-        <p class="fs-3"><strong>Speaker </strong>: Brother Trujillo</p>
+        <!--<p class="fs-3"><strong>Speaker </strong>: Brother Trujillo</p>
 
-        <p class="fs-3"><strong>Speaker </strong>: Elder Pang Siang</p>
+        <p class="fs-3"><strong>Speaker </strong>: Elder Pang Siang</p>-->
 
-        <p class="fs-3"><strong>Intermediate hymn </strong>: 219 Because I Have Been Given Much </p>
+        <!--<p class="fs-3"><strong>Intermediate hymn </strong>: 219 Because I Have Been Given Much </p>
 
-        <p class="fs-3"><strong>Speaker </strong>: Elder MeJean</p>
+        <p class="fs-3"><strong>Speaker </strong>: Elder MeJean</p>-->
 
-        <p class="fs-3"><strong>Closing Hymn </strong>: 222 Hear Thou Our Hymn, O Lord</p>
+        <p class="fs-3"><strong>Closing Hymn </strong>: 208 O Little Town of Bethlehem </p>
+
+        <p class="fs-3"><strong>Closing prayer </strong>: by invitation </p>
     </div>
 
     <div class="program">
         <h2>Announcements</h2>
-        <p class="fs-3"><strong>No announcements</strong></p>
+        <p class="fs-3"><strong>Abide by new national and stake Covid guidance on Sunday meetings and other gatherings</strong></p>
+        <p class="fs-3"><strong>Branch Council 12 Dec 08:00 </strong></p>
+        <p class="fs-3"><strong>Stake Youth Super Zoom activity: December 11 from 4:30 p.m. to 6 p.m.  All young people aged 12 to 18 are welcome! Come dressed in your best Christmas sweater. Zoom link by clicking <a href="https://zoom.us/j/91945779625?pwd=MmVJU0RTOHBWamtsQ0xCdFh0RU15Zz09">here</a></strong></p>
+        <p class="fs-3"><strong>Special video concert 'Witnesses for Christmas' which will be launched on 19 December 2021 at 6:00 PM CET. <br> Facebook link <a href="https://facebook.com/watch/173868825969090/1084965395589847">here</a> <span style="padding-left: 10%;"></span> Youtube link <a href="https://youtube.com/user/LDSPSDEU/videos">here</a> </strong></p>
+        <p class="fs-3"><strong>Tithing settlement by mid December. Sign up on branch Facebook page or contact brother Cole.</strong></p>
         <!-- <p class="fs-3"><strong>31 Oct </strong>: Fifth Sunday lesson (Adults/Youth) second hour</p>
 
         <p class="fs-3"><strong>6 Nov </strong>: <br>- 16:00 to 18:00 Stake leadership Meeting <br>- 18:30 Adult Session</p>
