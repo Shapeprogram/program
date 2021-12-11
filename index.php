ea4394bd099f7773aefac70082dc8c1bd6b32a16
Shapeprogram <93039490+Shapeprogram@users.noreply.github.com>
2021-12-11 20:57:36 +0100

    Add files via upload

--- index.php
+++ index.php
@@ -6,44 +6,35 @@ if(file_exists($head))
 }
 ?>
 
-    <h1>Church Program - 05 December 2021</h1>
+    <h1>Church Program - 12 December 2021</h1>
     <div class="program">
         <h2>Program</h2>
-        <p class="fs-3"><strong>Opening hymn </strong>: 202 Oh, Come, All Ye Faithful </p>
+        <p class="fs-3"><strong>Opening hymn </strong>: 213 The First Noel </p>
 
         <p class="fs-3"><strong>Opening prayer </strong> : By invitation</p>
 
-        <p class="fs-3"><strong>Sacrament hymn </strong>: 146 Gently Raise the Sacred Strain</p>
+        <p class="fs-3"><strong>Sacrament hymn </strong>: 197 O Savior, Thou Who Wearest a Crown</p>
 
-        <!--<p class="fs-3"><strong>Speaker </strong>: Brother Trujillo</p>
+        <p class="fs-3"><strong>Speaker </strong>: Mimi Kahne </p>
 
-        <p class="fs-3"><strong>Speaker </strong>: Elder Pang Siang</p>-->
+        <p class="fs-3"><strong>Speaker </strong>: Noelle Kahne</p>
 
-        <!--<p class="fs-3"><strong>Intermediate hymn </strong>: 219 Because I Have Been Given Much </p>
+        <p class="fs-3"><strong>Intermediate hymn </strong>: 203 Angels We Have Heard on High </p>
 
-        <p class="fs-3"><strong>Speaker </strong>: Elder MeJean</p>-->
+        <p class="fs-3"><strong>Speaker </strong>: Anthony Fernandes</p>
 
-        <p class="fs-3"><strong>Closing Hymn </strong>: 208 O Little Town of Bethlehem </p>
+        <p class="fs-3"><strong>Closing Hymn </strong>: 207 It Came Upon a Midnight Clear </p>
 
         <p class="fs-3"><strong>Closing prayer </strong>: By invitation </p>
     </div>
 
     <div class="program">
         <h2>Announcements</h2>
-        <p class="fs-3"><strong>Abide by new national and stake Covid guidance on Sunday meetings and other gatherings</strong></p>
-        <p class="fs-3"><strong>Branch Council 12 Dec 08:00 </strong></p>
-        <p class="fs-3"><strong>Stake Youth Super Zoom activity: December 11 from 4:30 p.m. to 6 p.m.  All young people aged 12 to 18 are welcome! Come dressed in your best Christmas sweater. Zoom link by clicking <a href="https://zoom.us/j/91945779625?pwd=MmVJU0RTOHBWamtsQ0xCdFh0RU15Zz09">here</a></strong></p>
-        <p class="fs-3"><strong>Special video concert 'Witnesses for Christmas' which will be launched on 19 December 2021 at 6:00 PM CET. <br> Facebook link <a href="https://facebook.com/watch/173868825969090/1084965395589847">here</a> <span style="padding-left: 10%;"></span> Youtube link <a href="https://youtube.com/user/LDSPSDEU/videos">here</a> </strong></p>
-        <p class="fs-3"><strong>Tithing settlement by mid December. Sign up on branch Facebook page or contact brother Cole.</strong></p>
-        <!-- <p class="fs-3"><strong>31 Oct </strong>: Fifth Sunday lesson (Adults/Youth) second hour</p>
-
-        <p class="fs-3"><strong>6 Nov </strong>: <br>- 16:00 to 18:00 Stake leadership Meeting <br>- 18:30 Adult Session</p>
-
-        <p class="fs-3"><strong>7 Nov </strong>: <br>- 10:00 to 12:00 Stake Conference Session for All <br>- 12:30 to 13:00 Temple and family history meeting (Temple /Family History leaders and consultants) <br>- 18:00 to 19:00 Fireside (Family History Devotional)</p>
-
-        <p class="fs-3"><strong>Donations </strong>: We would like to share with you an email that Alice and Marcel Bastiaens from the Brussels Center Ward recently sent:
-            “We would like to collect new or used toys from members, as well as Saint Nicholas and Christmas treats for affected children. “We will come and collect them in the units until November 15.”
-        </p> -->
+        <p class="fs-3">1. <strong>Today, Sunday 12 Dec, second hour Branch RS lesson for all Sisters. Brethren will take over Sunday lessons</strong></p>
+        <p class="fs-3">2. <strong>Stake fireside tonight, 12 Dec at 18:00. Zoom link by clicking <a href="https://zoom.us/j/92892449117?pwd=b0RzaXNDdzFiNEErSnFZejM4NGVHQT09">here</a>. Meeting ID: 928 9244 9117. Secret code: 1234 </strong></p>
+        <p class="fs-3">3. <strong>Special video concert 'Witnesses for Christmas' which will be launched on 19 December 2021 at 6:00 PM CET. <br> Facebook link <a href="https://facebook.com/watch/173868825969090/1084965395589847">here</a> <span style="padding-left: 10%;"></span> Youtube link <a href="https://youtube.com/user/LDSPSDEU/videos">here</a> </strong></p>
+        <p class="fs-3">4. <strong>19 Dec at 1900: Brussels stake Christmas concert. Broadcast only on the Internet (not face-to-face because of measures taken by the government following the Covid). Link will be communicated later.</strong></p>
+        <p class="fs-3">5. <strong>Tithing settlement by mid December. Sign up on branch Facebook page or contact brother Cole.</strong></p>
 
     </div>
     <!--
