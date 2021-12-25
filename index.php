2d84bef0ee1c5d506dd811efc16db248e84b7fc3
Shapeprogram <93039490+Shapeprogram@users.noreply.github.com>
2021-12-25 21:10:30 +0100

    Add files via upload

--- index.php
+++ index.php
@@ -5,55 +5,30 @@ if(file_exists($head))
   include($head);
 }
 ?>
-
-    <h1>Church Program - 19 December 2021</h1>
+    <h1>Church Program - 26 December 2021</h1>
     <div class="program">
         <h2>Program</h2>
-        <p class="fs-3"><strong>Opening hymn </strong>: 209 Hark! The Herald Angels Sing </p>
+        <p class="fs-3"><strong>Opening hymn </strong>: 207 It Came Upon a Midnight Clear  </p>
 
         <p class="fs-3"><strong>Opening prayer </strong> : By invitation</p>
 
-        <p class="fs-3"><strong>Sacrament hymn </strong>: 181 Jesus of Nazereth, Savior and King</p>
+        <p class="fs-3"><strong>Sacrament hymn </strong>: 172 In Humility, Our Savior</p>
 
-        <p class="fs-3"><strong>Scripture Reading </strong>: Julia Cole </p>
+        <p class="fs-3"><strong>Speaker </strong>: Melissa Heaton </p>
 
-        <p class="fs-3"><strong>Musical Number </strong>: Picture a Christmas, Primary Children </p>
+        <p class="fs-3"><strong>Intermediate hymn </strong>: 202 O Come All Ye Faithful  </p>
 
-        <p class="fs-3"><strong>Speaker </strong>: John Burton </p>
+        <p class="fs-3"><strong>Speaker </strong>: Ivo Marien</p>
 
-        <p class="fs-3"><strong>Musical Number </strong>: The First Noel, Gwen Roa and Kelli Burton </p>
-
-        <p class="fs-3"><strong>Speaker </strong>: Noelle Kahne</p>
-
-        <p class="fs-3"><strong>Congregational hymn </strong>: 201 Joy to the World </p>
-
-        <p class="fs-3"><strong>Speaker </strong>: Samantha Baami</p>
-
-        <p class="fs-3"><strong>Musical Number </strong>: Away in the Manger, Primary Children </p>
-
-        <p class="fs-3"><strong>Speaker </strong>: Bryan LeBaron</p>
-
-        <p class="fs-3"><strong>Musical Number </strong>: McKay Heaton and Kelli Burton </p>
-
-        <p class="fs-3"><strong>Closing remarks </strong>: President McCormick </p>
-
-        <p class="fs-3"><strong>Closing Hymn </strong>: 205 Once in Royal David's City </p>
+        <p class="fs-3"><strong>Closing Hymn </strong>: 210 With Wondering Awe</p>
 
         <p class="fs-3"><strong>Closing prayer </strong>: By invitation </p>
     </div>
 
     <div class="program">
         <h2>Announcements</h2>
-        <p class="fs-3">1. <strong>Special video concert 'Witnesses for Christmas' which will be launched on 19 December 2021 at 6:00 PM CET. <br> Facebook link <a href="https://facebook.com/watch/173868825969090/1084965395589847">here</a> <span style="padding-left: 10%;"></span> Youtube link <a href="https://youtube.com/user/LDSPSDEU/videos">here</a> </strong></p>
-        <p class="fs-3">2. <strong>19 Dec at 1900: Brussels stake Christmas concert. Broadcast only on the Internet (not face-to-face because of measures taken by the government following the Covid). Link will be communicated later.</strong></p>
-
+        <p class="fs-3"><!--1.--><strong>No announcements </strong></p>
     </div>
-    <!--
-    <div class="pdflink">
-        <p class="fs-4"> If the website isn't clear enough, you can download the PDF version of the program here : <a href="./doc/24OCT21-QR.pdf" class="btn btn-outline-info" download="pdf-shape-program" role="button">Download the PDF</a></p>
-    </div>
-    -->
-
 <?php 
 	$foot='./php/foot.php';
 	if(file_exists($foot))
