3ff9de1a876b9c75c9ed51ff950d87bcc23bd592
Shapeprogram <93039490+Shapeprogram@users.noreply.github.com>
2022-01-01 20:25:35 +0100

    Add files via upload

--- index.php
+++ index.php
@@ -8,19 +8,19 @@ if(file_exists($head))
     <h1>Church Program - 26 December 2021</h1>
     <div class="program">
         <h2>Program</h2>
-        <p class="fs-3"><strong>Opening hymn </strong>: 207 It Came Upon a Midnight Clear  </p>
+        <p class="fs-3"><strong>Opening hymn </strong>: 217 come let us anew   </p>
 
         <p class="fs-3"><strong>Opening prayer </strong> : By invitation</p>
 
-        <p class="fs-3"><strong>Sacrament hymn </strong>: 172 In Humility, Our Savior</p>
+        <p class="fs-3"><strong>Sacrament hymn </strong>: 188 thy will, o lord, be done </p>
 
-        <p class="fs-3"><strong>Speaker </strong>: Melissa Heaton </p>
+        <!--<p class="fs-3"><strong>Speaker </strong>: Melissa Heaton </p>
 
         <p class="fs-3"><strong>Intermediate hymn </strong>: 202 O Come All Ye Faithful  </p>
 
-        <p class="fs-3"><strong>Speaker </strong>: Ivo Marien</p>
+        <p class="fs-3"><strong>Speaker </strong>: Ivo Marien</p>-->
 
-        <p class="fs-3"><strong>Closing Hymn </strong>: 210 With Wondering Awe</p>
+        <p class="fs-3"><strong>Closing Hymn </strong>: 220 lord, i would follow thee</p>
 
         <p class="fs-3"><strong>Closing prayer </strong>: By invitation </p>
     </div>
