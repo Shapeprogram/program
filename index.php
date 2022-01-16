3afeafd85140683737bd425444d3fbbde5de2cf6
Shapeprogram <93039490+Shapeprogram@users.noreply.github.com>
2022-01-16 04:26:19 +0100

    Update index.php

--- index.php
+++ index.php
@@ -5,22 +5,22 @@ if(file_exists($head))
   include($head);
 }
 ?>
-    <h1>Church Program - 09 January 2022</h1>
+    <h1>Church Program - 16 January 2022</h1>
     <div class="program">
         <h2>Program</h2>
-        <p class="fs-3"><strong>Opening hymn </strong>: 9 - Come, Rejoice   </p>
+        <p class="fs-3"><strong>Opening hymn </strong>: 70 Sing to Praise Him    </p>
 
         <p class="fs-3"><strong>Opening prayer </strong> : By invitation</p>
 
-        <p class="fs-3"><strong>Sacrament hymn </strong>: 180 - Father in Heaven, We Do Believe  </p>
+        <p class="fs-3"><strong>Sacrament hymn </strong>: 176 ’Tis Sweet to Sing the Matchless Love   </p>
 
-        <p class="fs-3"><strong>Speaker </strong>: Sister Talevinukuleka  </p>
+        <p class="fs-3"><strong>Speaker </strong>: Sister Trujillo  </p>
 
-        <p class="fs-3"><strong>Intermediate hymn </strong>: 75 - In Hymns of Praise  </p>
+        <p class="fs-3"><strong>Intermediate hymn </strong>: 235 Should You Feel Inclined to Censure   </p>
 
-        <p class="fs-3"><strong>Speaker </strong>: Brother Talevinukuleka </p>
+        <p class="fs-3"><strong>Speaker </strong>: Brother Cole  </p>
 
-        <p class="fs-3"><strong>Closing Hymn </strong>: 128 - When Faith Endures</p>
+        <p class="fs-3"><strong>Closing Hymn </strong>: 45 Lead me into Eternal Life </p>
 
         <p class="fs-3"><strong>Closing prayer </strong>: By invitation </p>
     </div>
@@ -36,4 +36,3 @@ if(file_exists($head))
   		include($foot);
 	}
 ?>
-
