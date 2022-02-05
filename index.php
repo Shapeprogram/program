3834a4797fc792cbfef303bcbf20087a751199ea
Shapeprogram <93039490+Shapeprogram@users.noreply.github.com>
2022-02-05 23:24:12 +0100

    Add files via upload

--- index.php
+++ index.php
@@ -5,29 +5,35 @@ if(file_exists($head))
   include($head);
 }
 ?>
-    <h1>Church Program - 30 January 2022</h1>
+    <h1>Church Program - 06 February 2022</h1>
     <div class="program">
         <h2>Program</h2>
-        <p class="fs-3"><strong>Opening hymn </strong>: 147 Sweet Is the Work </p>
+        <p class="fs-3"><strong>Opening hymn </strong>: 246 Onward, Christian Soldiers </p>
 
         <p class="fs-3"><strong>Opening prayer </strong> : By invitation</p>
 
-        <p class="fs-3"><strong>Sacrament hymn </strong>: 191 Behold the Great Redeemer Die </p>
+        <p class="fs-3"><strong>Sacrament hymn </strong>: 175 O God, the Eternal Father </p>
 
-        <p class="fs-3"><strong>Speaker </strong>: Brother Rojas</p>
+        <!--<p class="fs-3"><strong>Speaker </strong>: Brother Rojas</p>
 
         <p class="fs-3"><strong>Intermediate hymn </strong>: 83 Guide Us, O Thou Great Jehovah</p>
 
-        <p class="fs-3"><strong>Speaker </strong>: Sister Trujillo</p>
+        <p class="fs-3"><strong>Speaker </strong>: Sister Trujillo</p>-->
 
-        <p class="fs-3"><strong>Closing Hymn </strong>: 157 Thy Spirit, Lord, Has Stirred Our Souls</p>
+        <p class="fs-3"><strong>Closing Hymn </strong>: 139 In Fasting We Approach Thee</p>
 
         <p class="fs-3"><strong>Closing prayer </strong>: By invitation </p>
     </div>
 
     <div class="program">
         <h2>Announcements</h2>
-        <p class="fs-3"><!--1.--><strong>No announcements </strong></p>
+        <p class="fs-3">1.<strong>Temple will be closed from 14th of February till 25th of April for maintenance reasons. </strong></p>
+        <p class="fs-3">2.<strong>Need for volunteers to chaperone workers in the Hague Temple from 14 February until 22 April. </strong></p>
+        <p class="fs-3">3.<strong>Youth Game night on 12 Feb </strong></p>
+        <p class="fs-3">4.<strong>Branch Council 13 Feb 08:00  </strong></p>
+        <p class="fs-3">5.<strong>Branch Youth Council 13 Feb after church </strong></p>
+        <p class="fs-3">6.<strong>Branch Conference on 20 Feb 2022 </strong></p>
+
     </div>
 <?php 
 	$foot='./php/foot.php';
