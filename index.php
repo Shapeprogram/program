a0b59c9ea1bbe5d7e1cb1e27e57194cf43d9548a
Shapeprogram <93039490+Shapeprogram@users.noreply.github.com>
2022-02-13 00:55:42 +0100

    Add files via upload

--- index.php
+++ index.php
@@ -5,22 +5,22 @@ if(file_exists($head))
   include($head);
 }
 ?>
-    <h1>Church Program - 06 February 2022</h1>
+    <h1>Church Program - 13 February 2022</h1>
     <div class="program">
         <h2>Program</h2>
-        <p class="fs-3"><strong>Opening hymn </strong>: 246 Onward, Christian Soldiers </p>
+        <p class="fs-3"><strong>Opening hymn </strong>: 42 Hail to the Brightness of Zion's Glad Morning </p>
 
         <p class="fs-3"><strong>Opening prayer </strong> : By invitation</p>
 
-        <p class="fs-3"><strong>Sacrament hymn </strong>: 175 O God, the Eternal Father </p>
+        <p class="fs-3"><strong>Sacrament hymn </strong>: 192 He Died! The Great Redeemer Died </p>
 
-        <!--<p class="fs-3"><strong>Speaker </strong>: Brother Rojas</p>
+        <p class="fs-3"><strong>Speaker </strong>: Motoko Shepherd </p>
 
-        <p class="fs-3"><strong>Intermediate hymn </strong>: 83 Guide Us, O Thou Great Jehovah</p>
+        <p class="fs-3"><strong>Intermediate hymn </strong>: 68 A Mighty Fortress Is Our God</p>
 
-        <p class="fs-3"><strong>Speaker </strong>: Sister Trujillo</p>-->
+        <p class="fs-3"><strong>Speaker </strong>: Doug Shepherd </p>
 
-        <p class="fs-3"><strong>Closing Hymn </strong>: 139 In Fasting We Approach Thee</p>
+        <p class="fs-3"><strong>Closing Hymn </strong>: 308 Love One Another</p>
 
         <p class="fs-3"><strong>Closing prayer </strong>: By invitation </p>
     </div>
@@ -29,10 +29,8 @@ if(file_exists($head))
         <h2>Announcements</h2>
         <p class="fs-3">1.<strong>Temple will be closed from 14th of February till 25th of April for maintenance reasons. </strong></p>
         <p class="fs-3">2.<strong>Need for volunteers to chaperone workers in the Hague Temple from 14 February until 22 April. </strong></p>
-        <p class="fs-3">3.<strong>Youth Game night on 12 Feb </strong></p>
-        <p class="fs-3">4.<strong>Branch Council 13 Feb 08:00  </strong></p>
-        <p class="fs-3">5.<strong>Branch Youth Council 13 Feb after church </strong></p>
-        <p class="fs-3">6.<strong>Branch Conference on 20 Feb 2022 </strong></p>
+        <p class="fs-3">3.<strong>Branch Youth Council 13 Feb after church </strong></p>
+        <p class="fs-3">3.<strong>Branch Conference on 20 Feb 2022 </strong></p>
 
     </div>
 <?php 
