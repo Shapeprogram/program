0d7e6c25e4a657eb444c3e697fedb645b391ae68
Shapeprogram <93039490+Shapeprogram@users.noreply.github.com>
2022-02-20 00:57:04 +0100

    Add files via upload

--- index.php
+++ index.php
@@ -5,32 +5,35 @@ if(file_exists($head))
   include($head);
 }
 ?>
-    <h1>Church Program - 13 February 2022</h1>
+    <h1>Church Program - 20 February 2022</h1>
     <div class="program">
         <h2>Program</h2>
-        <p class="fs-3"><strong>Opening hymn </strong>: 42 Hail to the Brightness of Zion's Glad Morning </p>
+        <p class="fs-3"><strong>Opening hymn </strong>: 38 Come, All Ye Saints of Zion </p>
 
         <p class="fs-3"><strong>Opening prayer </strong> : By invitation</p>
 
-        <p class="fs-3"><strong>Sacrament hymn </strong>: 192 He Died! The Great Redeemer Died </p>
+        <p class="fs-3"><strong>Sacrament hymn </strong>: 189 O Thou, Before the World Began </p>
 
-        <p class="fs-3"><strong>Speaker </strong>: Motoko Shepherd </p>
+        <p class="fs-3"><strong>Testimony </strong>: Brother Saimone Talevinukuleka  </p>
 
-        <p class="fs-3"><strong>Intermediate hymn </strong>: 68 A Mighty Fortress Is Our God</p>
+        <p class="fs-3"><strong>Testimony </strong>: Sister Alisha Talevinukuleka   </p>
 
-        <p class="fs-3"><strong>Speaker </strong>: Doug Shepherd </p>
+        <p class="fs-3"><strong>Intermediate hymn </strong>: 66 Rejoice, the Lord Is King!</p>
 
-        <p class="fs-3"><strong>Closing Hymn </strong>: 308 Love One Another</p>
+        <p class="fs-3"><strong>Speaker </strong>: Brother Paul Trujillo  </p>
 
-        <p class="fs-3"><strong>Closing prayer </strong>: By invitation </p>
+        <p class="fs-3"><strong>Closing Hymn </strong>: 152 God Be with You Till We Meet Again</p>
+
+        <p class="fs-3"><strong>Closing prayer </strong>: By invitation</p>
     </div>
 
     <div class="program">
         <h2>Announcements</h2>
-        <p class="fs-3">1.<strong>Temple will be closed from 14th of February till 25th of April for maintenance reasons. </strong></p>
-        <p class="fs-3">2.<strong>Need for volunteers to chaperone workers in the Hague Temple from 14 February until 22 April. </strong></p>
-        <p class="fs-3">3.<strong>Branch Youth Council 13 Feb after church </strong></p>
-        <p class="fs-3">4.<strong>Branch Conference on 20 Feb 2022 </strong></p>
+        <p class="fs-3">1.<strong>Temple will be closed from 14th of February till 25th of April for maintenance reasons.</strong></p>
+        <p class="fs-3">2.<strong>Need for volunteers to chaperone workers in the Hague Temple from 14 February until 22 April.</strong></p>
+        <p class="fs-3">3.<strong>Primary: Friend to Friend event available for streaming from 19 Feb.</strong></p>
+        <p class="fs-3">4.<strong>RootsTech 2022 virtual conference on March 3-5</strong></p>
+        <p class="fs-3">5.<strong>General Conference 2-3 April 22</strong></p>
 
     </div>
 <?php 
