8e7fa2474835cb61c9c8cc5582a0bee6161ea98c
Shapeprogram <93039490+Shapeprogram@users.noreply.github.com>
2022-03-06 00:03:25 +0100

    Add files via upload

--- index.php
+++ index.php
@@ -5,32 +5,30 @@ if(file_exists($head))
   include($head);
 }
 ?>
-    <h1>Church Program - 27 February 2022</h1>
+    <h1>Church Program - 06 March 2022</h1>
     <div class="program">
         <h2>Program</h2>
-        <p class="fs-3"><strong>Opening hymn </strong>: 73 Praise the Lord with Heart and Voice </p>
+        <p class="fs-3"><strong>Opening hymn </strong>: 7 Israel, Israel, God Is Calling </p>
 
         <p class="fs-3"><strong>Opening prayer </strong> : By invitation</p>
 
-        <p class="fs-3"><strong>Sacrament hymn </strong>: 180 Father in Heaven, We Do Believe </p>
+        <p class="fs-3"><strong>Sacrament hymn </strong>: 171 With Humble Heart </p>
 
-        <p class="fs-3"><strong>Testimony </strong>: Sister Elodie McCormick   </p>
+        <p class="fs-3"><strong>Testimonies </strong></p>
 
-        <p class="fs-3"><strong>Intermediate hymn </strong>: 67 Glory to God on High</p>
+        <!--<p class="fs-3"><strong>Intermediate hymn </strong>: 67 Glory to God on High</p>
 
-        <p class="fs-3"><strong>Speaker </strong>: President McCormick </p>
+        <p class="fs-3"><strong>Speaker </strong>: President McCormick </p>-->
 
-        <p class="fs-3"><strong>Closing Hymn </strong>: 98 I Need Thee Every Hour</p>
+        <p class="fs-3"><strong>Closing Hymn </strong>: 89 The Lord Is My Light</p>
 
         <p class="fs-3"><strong>Closing prayer </strong>: By invitation</p>
     </div>
 
     <div class="program">
         <h2>Announcements</h2>
-        <p class="fs-3">1.<strong> Need for volunteers to chaperone workers in the Hague Temple from 14 February until 22 April.</strong></p>
-        <p class="fs-3">2.<strong> RootsTech 2022 virtual conference on March 3-5</strong></p>
-        <p class="fs-3">3.<strong> General Conference 2-3 April 22</strong></p>
-
+        <p class="fs-3">1.<strong> Temple will be closed from 14th of February till 25th of April for maintenance reasons. </strong></p>
+        <p class="fs-3">2.<strong> General Conference 2-3 April 22</strong></p>
     </div>
 <?php 
 	$foot='./php/foot.php';
