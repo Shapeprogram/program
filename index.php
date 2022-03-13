7ce61858329b13fbf06248edd028fe0b1bf513dd
Shapeprogram <93039490+Shapeprogram@users.noreply.github.com>
2022-03-13 03:05:31 +0100

    Update index.php

--- index.php
+++ index.php
@@ -29,7 +29,7 @@ if(file_exists($head))
 
     <div class="program">
         <h2>Announcements</h2>
-        <p class="fs-3">1.<strong> Branch Temple Trip: Paris Temple, 26 March 2022/strong></p>
+        <p class="fs-3">1.<strong> Branch Temple Trip: Paris Temple, 26 March 2022</strong></p>
         <p class="fs-3">2.<strong> General Conference 2-3 April 22</strong></p>
         <p class="fs-3">3.<strong> The Hague Netherlands Temple will be closed until 25 April 2022</strong></p>
     </div>
