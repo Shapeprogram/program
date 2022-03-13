cf139bdb459d5cf25adc5fba5b6fefd5112b22c5
Shapeprogram <93039490+Shapeprogram@users.noreply.github.com>
2022-03-13 03:03:25 +0100

    Add files via upload

--- index.php
+++ index.php
@@ -5,30 +5,33 @@ if(file_exists($head))
   include($head);
 }
 ?>
-    <h1>Church Program - 06 March 2022</h1>
+    <h1>Church Program - 13 March 2022</h1>
     <div class="program">
         <h2>Program</h2>
-        <p class="fs-3"><strong>Opening hymn </strong>: 7 Israel, Israel, God Is Calling </p>
+        <p class="fs-3"><strong>Opening hymn </strong>: 1 The Morning Breaks</p>
 
         <p class="fs-3"><strong>Opening prayer </strong> : By invitation</p>
 
-        <p class="fs-3"><strong>Sacrament hymn </strong>: 171 With Humble Heart </p>
+        <p class="fs-3"><strong>Sacrament hymn </strong>: 170 God, Our Father, Hear Us Pray</p>
 
-        <p class="fs-3"><strong>Testimonies </strong></p>
+        <p class="fs-3"><strong>Speaker </strong>: Noémi Kahne </p>
 
-        <!--<p class="fs-3"><strong>Intermediate hymn </strong>: 67 Glory to God on High</p>
+        <p class="fs-3"><strong>Speaker </strong>: Lynzie Trujillo </p>
 
-        <p class="fs-3"><strong>Speaker </strong>: President McCormick </p>-->
+        <p class="fs-3"><strong>Intermediate hymn </strong>: 105 Master, the Tempest is Raging</p>
 
-        <p class="fs-3"><strong>Closing Hymn </strong>: 89 The Lord Is My Light</p>
+        <p class="fs-3"><strong>Speaker </strong>: Elder Wilkinson</p>
+
+        <p class="fs-3"><strong>Closing Hymn </strong>: 296 Our Father, by Whose Name</p>
 
         <p class="fs-3"><strong>Closing prayer </strong>: By invitation</p>
     </div>
 
     <div class="program">
         <h2>Announcements</h2>
-        <p class="fs-3">1.<strong> Temple will be closed from 14th of February till 25th of April for maintenance reasons. </strong></p>
+        <p class="fs-3">1.<strong> Branch Temple Trip: Paris Temple, 26 March 2022/strong></p>
         <p class="fs-3">2.<strong> General Conference 2-3 April 22</strong></p>
+        <p class="fs-3">3.<strong> The Hague Netherlands Temple will be closed until 25 April 2022</strong></p>
     </div>
 <?php 
 	$foot='./php/foot.php';
