621ff408ed605c2929117f691c8d5bf23d705b46
Shapeprogram <93039490+Shapeprogram@users.noreply.github.com>
2022-04-17 09:06:43 +0200

    Add files via upload

--- index.php
+++ index.php
@@ -5,30 +5,34 @@ if(file_exists($head))
   include($head);
 }
 ?>
-    <h1>Church Program - 19 March 2022</h1>
+    <h1>Church Program - 17 April 2022</h1>
     <div class="program">
         <h2>Program</h2>
-        <p class="fs-3"><strong>Opening hymn </strong>: 41 Let Zion in Her Beauty Rise</p>
+        <p class="fs-3"><strong>Opening hymn </strong>: 199 He Is Risen!</p>
 
         <p class="fs-3"><strong>Opening prayer </strong> : By invitation</p>
 
-        <p class="fs-3"><strong>Sacrament hymn </strong>: 188 Thy Will, O Lord Be Doney</p>
+        <p class="fs-3"><strong>Sacrament hymn </strong>: 197 O Savior, Thou Who Wearest a Crown</p>
 
-        <p class="fs-3"><strong>Speaker </strong>: Sister Roa </p>
+        <p class="fs-3"><strong>The Living Christ Reading:</strong>: Brennan Cole, Celeste Palmer, Noemie Kahne</p>
 
-        <p class="fs-3"><strong>Intermediate hymn </strong>: 221 Dear to the Heart of the Shepherd</p>
+        <p class="fs-3"><strong>Musical Number</strong>: Primary Children: Because </p>
 
-        <p class="fs-3"><strong>Speaker </strong>: Brother Roa</p>
+        <p class="fs-3"><strong>Speaker </strong>: Santi Rojas</p>
 
-        <p class="fs-3"><strong>Closing Hymn </strong>: 69 All Glory, Laud, and Honor</p>
+        <p class="fs-3"><strong>Musical Number</strong>: McKay Heaton on Trombone</p>
+
+        <p class="fs-3"><strong>Speaker </strong>: Melissa Heaton</p>
+
+        <p class="fs-3"><strong>Closing Hymn </strong>: 200 Christ the Lord Is Risen Today </p>
 
         <p class="fs-3"><strong>Closing prayer </strong>: By invitation</p>
     </div>
 
     <div class="program">
         <h2>Announcements</h2>
-        <p class="fs-3">1.<strong> Branch Temple Trip March 26th, Paris Temple</strong></p>
-        <p class="fs-3">2.<strong> General Conference 2-3 April 22</strong></p>
+        <p class="fs-3">1.<strong> 24 April: Fireside with BYU Ballroom Dancers troupe 5 p.m. in Grimbergen</strong></p>
+        <p class="fs-3">2.<strong> 21 May: Stake Primary Activity, 2 sessions: 10:00-11:00/11:30-12:30. Must register by 21 April. </strong></p>
         <!--<p class="fs-3">3.<strong> The Hague Netherlands Temple will be closed until 25 April 2022</strong></p>-!>
     </div>
 <?php 
