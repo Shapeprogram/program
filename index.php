d6aca612c0280b58fb150d290002fc6c3a0a9a6b
Shapeprogram <93039490+Shapeprogram@users.noreply.github.com>
2022-09-17 23:29:19 +0200

    Update index.php

--- index.php
+++ index.php
@@ -5,35 +5,30 @@ if(file_exists($head))
   include($head);
 }
 ?>
-    <h1>Church Program - 17 April 2022</h1>
+    <h1>Church Program - 18 September 2022</h1>
     <div class="program">
         <h2>Program</h2>
-        <p class="fs-3"><strong>Opening hymn </strong>: 199 He Is Risen!</p>
+        <p class="fs-3"><strong>Opening hymn </strong>: TBD</p>
 
-        <p class="fs-3"><strong>Opening prayer </strong> : By invitation</p>
+        <p class="fs-3"><strong>Opening prayer </strong>: By invitation</p>
 
-        <p class="fs-3"><strong>Sacrament hymn </strong>: 197 O Savior, Thou Who Wearest a Crown</p>
+        <p class="fs-3"><strong>Sacrament hymn </strong>: TBD</p>
 
-        <p class="fs-3"><strong>The Living Christ Reading:</strong>: Brennan Cole, Celeste Palmer, Noemie Kahne</p>
+        <p class="fs-3"><strong>Speaker </strong>: Ivy Stine</p>
 
-        <p class="fs-3"><strong>Musical Number</strong>: Primary Children: Because </p>
+        <p class="fs-3"><strong>Intermediate Hymn </strong>: TBD</p>
 
-        <p class="fs-3"><strong>Speaker </strong>: Santi Rojas</p>
+        <p class="fs-3"><strong>Speaker </strong>: Greg Stine</p>
 
-        <p class="fs-3"><strong>Musical Number</strong>: McKay Heaton on Trombone</p>
-
-        <p class="fs-3"><strong>Speaker </strong>: Melissa Heaton</p>
-
-        <p class="fs-3"><strong>Closing Hymn </strong>: 200 Christ the Lord Is Risen Today </p>
+        <p class="fs-3"><strong>Closing Hymn </strong>: TBD</p>
 
         <p class="fs-3"><strong>Closing prayer </strong>: By invitation</p>
     </div>
 
     <div class="program">
         <h2>Announcements</h2>
-        <p class="fs-3">1.<strong> 24 April: Fireside with BYU Ballroom Dancers troupe 5 p.m. in Grimbergen</strong></p>
-        <p class="fs-3">2.<strong> 21 May: Stake Primary Activity, 2 sessions: 10:00-11:00/11:30-12:30. Must register by 21 April. </strong></p>
-        <!--<p class="fs-3">3.<strong> The Hague Netherlands Temple will be closed until 25 April 2022</strong></p>-!>
+        <p class="fs-3">1.<strong> Linger Longer After Church</strong></p>
+        <p class="fs-3">2.<strong> General Conference 1-2 October</strong></p>
     </div>
 <?php 
 	$foot='./php/foot.php';
@@ -42,4 +37,3 @@ if(file_exists($head))
   		include($foot);
 	}
 ?>
-
