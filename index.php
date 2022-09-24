49078dc5b75a188c699a66e34c8db19c32d3f39d
Shapeprogram <93039490+Shapeprogram@users.noreply.github.com>
2022-09-24 22:45:09 +0200

    Update index.php

--- index.php
+++ index.php
@@ -5,30 +5,31 @@ if(file_exists($head))
   include($head);
 }
 ?>
-    <h1>Church Program - 18 September 2022</h1>
+    <h1>Church Program - 25 September 2022</h1>
     <div class="program">
         <h2>Program</h2>
-        <p class="fs-3"><strong>Opening hymn </strong>: TBD</p>
+        <p class="fs-3"><strong>Opening hymn </strong>: #2 the Spirit of God</p>
 
         <p class="fs-3"><strong>Opening prayer </strong>: By invitation</p>
 
-        <p class="fs-3"><strong>Sacrament hymn </strong>: TBD</p>
+        <p class="fs-3"><strong>Sacrament hymn </strong>: #175 O God, the Eternal Father</p>
+	    <p class="fs-3"> The Primary Program</p>
 
-        <p class="fs-3"><strong>Speaker </strong>: Ivy Stine</p>
+        <!--<p class="fs-3"><strong>Speaker </strong>: Ivy Stine</p>
 
         <p class="fs-3"><strong>Intermediate Hymn </strong>: TBD</p>
 
-        <p class="fs-3"><strong>Speaker </strong>: Greg Stine</p>
+        <p class="fs-3"><strong>Speaker </strong>: Greg Stine</p>-->
 
-        <p class="fs-3"><strong>Closing Hymn </strong>: TBD</p>
+        <p class="fs-3"><strong>Closing Hymn </strong>: #239 Choose the Right</p>
 
         <p class="fs-3"><strong>Closing prayer </strong>: By invitation</p>
     </div>
 
     <div class="program">
         <h2>Announcements</h2>
-        <p class="fs-3">1.<strong> Linger Longer After Church</strong></p>
-        <p class="fs-3">2.<strong> General Conference 1-2 October</strong></p>
+        <!--<p class="fs-3">1.<strong> Linger Longer After Church</strong></p>
+        <p class="fs-3">2.<strong> General Conference 1-2 October</strong></p>-->
     </div>
 <?php 
 	$foot='./php/foot.php';
