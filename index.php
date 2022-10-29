85a472f16f4d554d2a975516e438a8b3290eac44
Shapeprogram <93039490+Shapeprogram@users.noreply.github.com>
2022-10-30 01:24:53 +0200

    Update index.php

--- index.php
+++ index.php
@@ -5,23 +5,25 @@ if(file_exists($head))
   include($head);
 }
 ?>
-    <h1>Church Program - 23 October 2022</h1>
+    <h1>Church Program - 30 October 2022</h1>
     <div class="program">
         <h2>Program</h2>
-        <p class="fs-3"><strong>Opening hymn </strong>: #85 How Firm a Foundation </p>
+        <p class="fs-3"><strong>Opening hymn </strong>: #3 Now Let Us Rejoice </p>
 
         <p class="fs-3"><strong>Opening prayer </strong>: By invitation</p>
 
-        <p class="fs-3"><strong>Sacrament hymn </strong>: #191 Behold the Great Redeemer Die </p>
+        <p class="fs-3"><strong>Sacrament hymn </strong>: #193 I Stand All Amazed </p>
 	    
 
-        <p class="fs-3"><strong>Speaker </strong>: Elder Ellis</p>
+        <p class="fs-3"><strong>Speaker </strong>: Brother Kirk Wright</p>
+	    
+	    <p class="fs-3"><strong>Speaker </strong>: Sister Winnie Burton</p>
 
-        <p class="fs-3"><strong>Intermediate Hymn </strong>: #304 Teach Me To Walk in the Light</p>
+        <p class="fs-3"><strong>Intermediate Hymn </strong>: #89 The Lord Is My Light</p>
 
-        <p class="fs-3"><strong>Speaker </strong>: Elder Boucaut</p>
+        <p class="fs-3"><strong>Speaker </strong>: Brother Kent Burton</p>
 
-        <p class="fs-3"><strong>Closing Hymn </strong>: #220 Lord I Would Follow Thee</p>
+        <p class="fs-3"><strong>Closing Hymn </strong>: #143 Let the Holy Spirit Guide</p>
 
         <p class="fs-3"><strong>Closing prayer </strong>: By invitation</p>
     </div>
@@ -29,7 +31,8 @@ if(file_exists($head))
     <div class="program">
         <h2>Announcements</h2>
         <!--<p class="fs-3">1.<strong> Linger Longer After Church</strong></p>-->
-        <p class="fs-3">1.<strong>12 Nov - Branch Temple Trip - Sis Talevinukuleka emailed sign up sheets. </strong></p> 
+        <p class="fs-3">1.<strong>Stake Conference 5 - 6 October  </strong></p> 
+	    <p class="fs-3">2.<strong>Branch Temple Trip 12 November</strong></p> 
     </div>
 <?php 
 	$foot='./php/foot.php';
